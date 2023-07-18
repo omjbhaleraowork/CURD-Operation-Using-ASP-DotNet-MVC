a702d3725fd1e6d1d43e85aa0137c32aecd7ad65
om <omjbhaleraowork.com>
2023-07-18 13:00:33 +0530

    Add project files.

---
+++ Controllers/productcontroller.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LabExamSolution.Controllers
+{
+    public class ProductController : Controller
+    {
+        private static List<Product> products = new List<Product>
+    {
+        new Product { ProductId = 101, ProductName = "Harry Porter", Rate = 10.00M, Description = "descA", CategoryName = "Books" },
+        new Product { ProductId = 102, ProductName = "Percy Jackson", Rate = 20.00M, Description = "descB", CategoryName = "Books" },
+        new Product { ProductId = 103, ProductName = "Pen", Rate = 30.00M, Description = "descC", CategoryName = "Stationery" },
+        new Product { ProductId = 104, ProductName = "Marker", Rate = 30.00M, Description = "descD", CategoryName = "Stationery" }
+    };
+
+        public ActionResult Index()
+        {
+            return View(products);
+        }
+
+        public ActionResult Edit(int productId)
+        {
+            var product = products.FirstOrDefault(p => p.ProductId == productId);
+            if (product == null)
+            {
+                return View();
+            }
+            return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult Update(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                var existingProduct = products.FirstOrDefault(p => p.ProductId == product.ProductId);
+                if (existingProduct != null)
+                {
+                    existingProduct.ProductName = product.ProductName;
+                    existingProduct.Rate = product.Rate;
+                    existingProduct.Description = product.Description;
+                    existingProduct.CategoryName = product.CategoryName;
+                }
+                return RedirectToAction("Index");
+            }
+            return View("Edit", product);
+        }
+
+        public ActionResult Delete(int productId)
+        {
+            var product = products.FirstOrDefault(p => p.ProductId == productId);
+            if (product == null)
+            {
+                return View();
+            }
+            return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult DeleteConfirmed(int productId)
+        {
+            var product = products.FirstOrDefault(p => p.ProductId == productId);
+            if (product != null)
+            {
+                products.Remove(product);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/product.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class Product
+{
+    public int ProductId { get; set; }
+
+    [Display(Name = "Product Name")]
+    [Required(ErrorMessage = "Product Name is required.")]
+    public string ProductName { get; set; }
+
+    [Required(ErrorMessage = "Rate is required.")]
+    public decimal Rate { get; set; }
+
+    [Required(ErrorMessage = "Description is required.")]
+    public string Description { get; set; }
+
+    [Display(Name = "Category")]
+    [Required(ErrorMessage = "Category is required.")]
+    public string CategoryName { get; set; }
+}
